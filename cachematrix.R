9ebf0a7f2193691fac4888c86c2c378756d57e5b
rednaic <66297142+rednaic@users.noreply.github.com>
2020-06-27 11:42:56 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
-##For this assignment I have assumed that the matrix supplied is always invertible.
+## For this assignment I have assumed that the matrix supplied is always invertible.
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   j <- NULL
@@ -14,7 +15,11 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
-##Please include your own comment to explain your code (Required in Rubric)
+## cacheSolve computes the inverse of the special "matrix" 
+	## returned by makeCacheMatrix above. If the inverse has already been calculated
+	## (and the matrix has not changed), then the cachesolve should retrieve
+	## the inverse from the cache.
+
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
